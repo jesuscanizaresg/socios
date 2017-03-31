bd714763c5efb89f0748e54965e63350406b2ab8
Javier Moreno <javier.moreno.vilaplana@gmail.com>
2017-03-31 11:58:40 +0200

    Clase casi acabada, sin implementrar el calendar

--- socio.java
+++ socio.java
@@ -5,5 +5,21 @@ class socio implements Serializable
 	private String apellido;
 	private String apellido2;
 	private String nif;
-	private GFregorianCalendar
+	private GFregorianCalendar nacimiento;
+	//constructor
+	public socio (String nombre, String apellido, String apellido2, String nif, String nacimiento)
+	{
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.apellido2=apellido2;
+		this.nif=nif;
+		this.nacimiento=nacimiento;
+	}
+	public void mostrar()
+	{
+		System.out.println("Nombre: "+nombre);
+		System.out.println("Primer apellido "+apellido);
+		System.out.println("Segundo apellido "+apellido2);
+		System.out.println("NIF/DNI: "+nif);
+	}
 }
